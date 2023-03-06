dbdbcb778dd91bad31298dbff4c70a892b3f7449
Enes Doğan <enesdoganlive@hotmail.com>
2023-03-06 14:02:17 +0300

    Added questions

---
+++ js/script.js
@@ -0,0 +1,76 @@
+const questionsData = [
+  {
+    question: "What is the most popular programming language in 2023?",
+    options: ["Javascript", "Java", "C#", "Python", "Golang"],
+    answer: "Javascript",
+  },
+  {
+    question: "What was the first web browser?",
+    options: [
+      "Netscape Navigator",
+      "Internet Explorer",
+      "Google Chrome",
+      "Firefox",
+      "Yandex",
+    ],
+    answer: "Netscape Navigator",
+  },
+  {
+    question: "What is the release date of Javascript?",
+    options: ["1995", "1996", "1997", "1998", "1999"],
+    answer: "1996",
+  },
+  {
+    question: "Which is a CSS property?",
+    options: ["for loop", "if statement", "flexbox", "while", "switch-case"],
+    answer: "flexbox",
+  },
+  {
+    question: "Which is the Javascript library?",
+    options: ["React", "Angular", "Vue", "Ember", "Blazor"],
+    answer: "React",
+  },
+  {
+    question: "What is the most popular database management system?",
+    options: ["MySQL", "Oracle", "SQL Server", "PostgreSQL", "SQLLite"],
+    answer: "MySQL",
+  },
+  {
+    question: "Which is not a backend technologies approach?",
+    options: ["MERN", "MEVN", "LAMP", ".NET", "Tensorflow"],
+    answer: "Tensorflow",
+  },
+  {
+    question: "What does HTTP stand for?",
+    options: [
+      "Hypertext Transfer Protocol",
+      "Hyper Text Transmission Protocol",
+      "Hyper Transfer Transmission Protocol",
+      "Hyper Text Transfer Processes",
+      "Hyper Transfer Transmission Processes",
+    ],
+    answer: "Hypertext Transfer Protocol",
+  },
+  {
+    question: 'What is the long form of  "CRUD?"',
+    options: [
+      "CREATE REMOVE UPDATE DELETE",
+      "CREATE READ UPDATE DELETE",
+      "CREATE REMOVE UPDATE DATA",
+      "CREATE RESET UPDATE DELETE",
+      "CREATE RESET UPDATE DATA",
+    ],
+    answer: "CREATE READ UPDATE DELETE",
+  },
+  {
+    question: "What is the long form of NoSQL?",
+    options: [
+      "Not only SQL",
+      "No SQL",
+      "Not One SQL",
+      "Notation SQL",
+      "Number One SQL",
+    ],
+    answer: "Not only SQL",
+  },
+];
