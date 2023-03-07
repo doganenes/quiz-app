24cdd8dbdfe1ecff4c8d0856be1b57975022248d
Enes Doğan <enesdoganlive@hotmail.com>
2023-03-07 17:54:51 +0300

    Modified Welcome Page

--- js/script.js
+++ js/script.js
@@ -76,11 +76,16 @@ const questionsData = [
 ];
 
 let welcomeMessage = document.getElementById("welcomeMessage");
-const colors = ["#030ffc", "#6e051c", "#1ba112", "#d02b", "#f542bc"];
+const codesArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, "A", "B", "C", "D", "E", "F"];
 
-let i = 0;
+const randomTextColor = () => {
+  let hexCode = "#";
+  for (let i = 0; i < 6; i++) {
+    hexCode += codesArr[Math.floor(Math.random() * codesArr.length)];
+  }
+  return hexCode;
+};
+
 setInterval(() => {
-  i = i % colors.length;
-  welcomeMessage.style.color = colors[i];
-  i++;
+  welcomeMessage.style.color = randomTextColor();
 }, 500);
